e0044ac499b7e718f8ce4653385c533f147efcf4
KLAIIIII <guyyg03546@gmail.com>
2022-06-02 17:12:18 +0700

    update

---
+++ bin/my_week3.dart
@@ -0,0 +1,11 @@
+void main(){
+  List<String> province = ['Tart', 'Chantaburi' , 'Rayong'];
+  //province[index,0] , 1  ,2
+  print(province[0]);
+  print(province[1]);
+  print(province[2]);
+  print("=========");
+  for(var s in province){
+    print(s);
+  }
+}
